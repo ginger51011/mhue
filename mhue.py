bcd39748cae3d0748c3df0aeb4051abac541c3e0
Emil Jonathan Eriksson <8700261+ginger51011@users.noreply.github.com>
2025-08-30 00:38:40 +0200

    Fixes already

--- mhue.py
+++ mhue.py
@@ -26,47 +26,47 @@ M_LETTER_SPACE = 3 * M_DOT  # Space between letters in a word
 M_SPACE = 7 * M_DOT
 
 M = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "V": "...-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
     # Swedish
-    "Å": ".--.-",
-    "Ä": ".-.-",
-    "Ö": "---.",
+    'Å': '.--.-',
+    'Ä': '.-.-',
+    'Ö': '---.',
     # Numbers
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "0": "-----",
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
 }
 
 
@@ -75,9 +75,9 @@ def translate(msg: str) -> str:
     words = msg.split()
     morse_words = []
     for word in words:
-        morse_word = ""
+        morse_word = ''
         for c in word:
-            morse_word += M.get(c, "")
+            morse_word += M.get(c, '')
         morse_words.append(morse_word)
     return morse_words
 
@@ -90,51 +90,51 @@ class Controller:
     @staticmethod
     def from_json_path(path: str) -> Self | None:
         if not os.path.exists(path):
-            print(f"ERROR: Did not find config file at {path}")
+            print(f'ERROR: Did not find config file at {path}')
             return None
-        with open(path, encoding="utf-8") as f:
+        with open(path, encoding='utf-8') as f:
             j = f.read()
             return Controller(**json.loads(j))
 
     def save(self, path: str):
         json_config = json.dumps(asdict(self), indent=4)
-        with open(path, "w", encoding="utf-8") as f:
+        with open(path, 'w', encoding='utf-8') as f:
             f.write(json_config)
 
     def base_url(self) -> str:
-        return f"http://{self.ip_address}/api/{self.username}"
+        return f'http://{self.ip_address}/api/{self.username}'
 
     def list_lamps(self):
-        res = req.get(f"{self.base_url()}/lights")
+        res = req.get(f'{self.base_url()}/lights')
         res.raise_for_status()
         for n, lamp in res.json().items():
             print(f'{n}: {lamp["name"]}')
 
     def set_lamp(self, lamp_id: int, on: bool):
         res = req.put(
-            f"{self.base_url()}/lights/{lamp_id}/state",
-            json={"on": on, "transitiontime": 2},
+            f'{self.base_url()}/lights/{lamp_id}/state',
+            json={'on': on, 'transitiontime': 2},
         )
         res.raise_for_status()
-        contains_hue_error(res.json(), context="set_lamp")
+        contains_hue_error(res.json(), context='set_lamp')
 
     def blink(self, lamp_id: int, duration_s: float):
         self.set_lamp(lamp_id, on=True)
         sleep(duration_s)
         self.set_lamp(lamp_id, on=False)
 
-    def blink_morse_word(self, lamp_id: int, morse_word: list[Literal[".", "-"]]):
+    def blink_morse_word(self, lamp_id: int, morse_word: list[Literal['.', '-']]):
         for i, c in enumerate(morse_word):
-            if c == ".":
+            if c == '.':
                 self.blink(lamp_id, M_DOT)
-            elif c == "-":
+            elif c == '-':
                 self.blink(lamp_id, M_DASH)
             # All except last one
             if i < len(morse_word) - 1:
                 sleep(M_LETTER_SPACE)
 
     def blink_morse_message(
-        self, lamp_id: int, morse_msg: list[list[Literal[".", "-"]]]
+        self, lamp_id: int, morse_msg: list[list[Literal['.', '-']]]
     ):
         """Prints a Morse message, divided into words (or special characters)."""
         for i, word in enumerate(morse_msg):
@@ -144,41 +144,41 @@ class Controller:
                 sleep(M_SPACE)
 
 
-def contains_hue_error(json: dict, context="unkown") -> bool:
+def contains_hue_error(json: dict, context='unkown') -> bool:
     """Checks JSON for a Hue error, prints it, and returns if an error was found."""
-    if len(json) > 0 and json[0].get("error") is not None:
+    if len(json) > 0 and json[0].get('error') is not None:
         print(f'ERROR: Bridge responded with {json[0]["error"]} (context: {context})')
         return True
     return False
 
 
 def default_config_path() -> str:
-    if (c := os.environ.get("XDG_CONFIG_HOME")) is not None:
-        return os.path.join(c, "mhue.json")
-    elif (c := os.environ.get("HOME")) is not None:
-        return os.path.join(c, ".mhue.json")
+    if (c := os.environ.get('XDG_CONFIG_HOME')) is not None:
+        return os.path.join(c, 'mhue.json')
+    elif (c := os.environ.get('HOME')) is not None:
+        return os.path.join(c, '.mhue.json')
     else:
-        return ".mhue.json"
+        return '.mhue.json'
 
 
 def handshake(ip: str) -> str | None:
     res = req.post(
-        f"http://{ip}/api",
+        f'http://{ip}/api',
         json={
-            "devicetype": "mhue client",
+            'devicetype': 'mhue client',
         },
     )
 
-    input("Press the button on your Hue Bridge now; press <Enter> when done")
+    input('Press the button on your Hue Bridge now; press <Enter> when done')
 
     res.raise_for_status()
 
     json = res.json()
 
-    if contains_hue_error(json, context="handshake"):
+    if contains_hue_error(json, context='handshake'):
         return None
     elif len(json) > 0:
-        return json[0].get("success", {}).get("username", None)
+        return json[0].get('success', {}).get('username', None)
     else:
         return None
 
@@ -191,69 +191,69 @@ def setup(ip: str, config_path: str) -> bool:
     c.save(config_path)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        prog="mhue",
-        description="Sends Morse code messages using your Philips Hue lamps",
+        prog='mhue',
+        description='Sends Morse code messages using your Philips Hue lamps',
         add_help=True,
     )
     parser.add_argument(
-        "-s",
-        "--setup",
-        nargs="?",
+        '-s',
+        '--setup',
+        nargs='?',
         type=str,
-        metavar="IP",
-        help="Setup an application with the provided Hue Bridge IP address and saves a configuration (see --output)",
+        metavar='IP',
+        help='Setup an application with the provided Hue Bridge IP address and saves a configuration (see --output)',
     )
     parser.add_argument(
-        "-o",
-        "--output",
-        nargs="?",
+        '-o',
+        '--output',
+        nargs='?',
         type=str,
-        metavar="PATH",
-        help="Where to save the configuration file when used with --setup.",
+        metavar='PATH',
+        help='Where to save the configuration file when used with --setup.',
         default=default_config_path(),
     )
     parser.add_argument(
-        "-t",
-        "--text",
-        nargs="?",
+        '-t',
+        '--text',
+        nargs='?',
         type=str,
-        metavar="TEXT",
-        help="Text to display",
+        metavar='TEXT',
+        help='Text to display',
     )
     (
         parser.add_argument(
-            "-d",
-            "--id",
-            nargs="?",
+            '-i',
+            '--id',
+            nargs='?',
             type=int,
-            metavar="ID",
-            help="Lamp ID to display --text on",
+            metavar='ID',
+            help='Lamp ID to display --text on',
         ),
     )
     parser.add_argument(
-        "-l",
-        "--list",
-        action="store_true",
-        help="List lamp IDs",
+        '-l',
+        '--list',
+        action='store_true',
+        help='List lamp IDs',
     )
     parser.add_argument(
-        "-c",
-        "--config-file",
-        nargs="?",
+        '-c',
+        '--config-file',
+        nargs='?',
         type=str,
-        metavar="PATH",
-        help="Path to config file",
+        metavar='PATH',
+        help='Path to config file',
         default=default_config_path(),
     )
     parser.add_argument(
-        "-r",
-        "--repeat",
-        nargs="?",
+        '-r',
+        '--repeat',
+        nargs='?',
         type=int,
-        metavar="N",
-        help="Repeat message N times",
+        metavar='N',
+        help='Repeat message N times',
         default=1,
     )
 
